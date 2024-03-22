3f67b81d6107da07d5446913a8877266f7c6f87f
arjunprratap05 <108358230+arjunprratap05@users.noreply.github.com>
2024-03-22 11:34:42 +0530

    PracticeCode

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
     const handleSubmit = (e) => {
         e.preventDefault()
         setUser({username, password})
-    }
+    } 
   return (
     <div>
         <h2>Login</h2>
